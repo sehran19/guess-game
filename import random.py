74cdedcc4808ae76d27e47e4de2b1348f3091d01
sehran19 <sehranratherrather@gmail.com>
2026-04-17 20:18:58 +0530

    update code

--- import random.py
+++ import random.py
@@ -1,13 +1,15 @@
 import random
-number = random.randint(1,50)
+number = random.randint(1, 50)
 guess=0
-print=("guess a number between 1 to 50")
-while guess!=number:
-    guess=int(input("enter your guess:"))
-    if guess < number:
-        print("too low! try again.")
+
+print("guess a number between 1 and 50")
+
+while guess != number:
+    guess = int(input("enter your guess:"))
+    if  guess < number:
+         print("too low! try again.")
     elif guess > number:
-        print("too high!try again.")
+         print("too high!try again.")
     else:
-        print("congratulations! you guessed it ")
+         print("congratulations! you guessed it ")
 
